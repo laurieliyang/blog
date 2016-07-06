e27e709809041ea5d714c572fbf97116e3e7ae56
李阳（Laurie） <liyang@liangzijinfu.com>
2016-07-06 16:40:19 +0800

    something new

--- articles.js
+++ articles.js
@@ -2,7 +2,7 @@ var articles = {
   "2016-07" : [
 	  {
 	  	"title":"从0到1太难——Python从0到0.6简明教程",
-	  	"file":"/2016_07/0_1_Python_0_0.6_.html"
+	  	"file":"2016_07/0_1_Python_0_0.6_.html"
 	  }
   ]
 };
